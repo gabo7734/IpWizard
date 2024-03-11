c2112c5ae8b695bc1f27cbddf90862d8bb90c5e5
Maria Paola Sanchez Castro <maria.sanchezc@ibm.com>
2024-03-11 12:49:32 -0600

    fixed name header

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ function App() {
   return (
     <div>
       <Header >
-        <HeaderName href="#" prefix="IBM"> IP Wizard </HeaderName>
+        <HeaderName href="#" prefix="IBM"> IPWizard </HeaderName>
         <HeaderNavigation aria-label="IBM [Platform]">
         </HeaderNavigation>
       </Header>
